7fc5b7c06b68f750421b58c5ece5f5eb2843d90f
Roman Volodin <roman.volodin@gmail.com>
2021-09-09 08:08:22 +0300

    Add comic comment fetching

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from xckd import save_image_from_url
+from xckd import save_image_from_url, fetch_comic_comment
 
 
 if __name__ == "__main__":
-    save_image_from_url("https://imgs.xkcd.com/comics/python.png", "python.png")
+    save_image_from_url(
+        "https://imgs.xkcd.com/comics/python.png", "python.png"
+    )
+    fetch_comic_comment("https://xkcd.com/353/info.0.json")
--- xckd.py
+++ xckd.py
@@ -6,3 +6,9 @@ def save_image_from_url(url, save_path, params=None):
     response.raise_for_status()
     with open(save_path, "wb") as image:
         image.write(response.content)
+
+
+def fetch_comic_comment(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()["alt"]
