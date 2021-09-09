543447f3a6cdbe060bba68ebe1f0be64fe2a50e3
Roman Volodin <roman.volodin@gmail.com>
2021-09-09 13:47:37 +0300

    Upload random comic on each script run.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 from environs import Env
 
 import vk
+from xckd import fetch_random_comic, save_image_from_url
 
 
 def main():
@@ -8,7 +10,16 @@ def main():
     env.read_env()
 
     vk_token = env.str("VK_ACCESS_TOKEN")
-    upload_url = vk.get_upload_url(vk_token)
+
+    image_path = "tmp.png"
+
+    comic = fetch_random_comic()
+    save_image_from_url(comic["img"], image_path)
+
+    uploaded_photo = vk.upload_image(vk_token, image_path)
+    os.remove(image_path)
+    saved_photo = vk.save_album_photo(vk_token, uploaded_photo)
+    vk.publish_photo(vk_token, saved_photo)
 
 
 if __name__ == "__main__":
