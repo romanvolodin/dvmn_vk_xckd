42a4af31b524fb0e6cb57492e5b97c8ad53e7fff
Roman Volodin <roman.volodin@gmail.com>
2021-09-09 14:47:02 +0300

    Remove unused code

--- vk.py
+++ vk.py
@@ -6,20 +6,6 @@ API_VERSION = 5.131
 VK_GROUP_ID = 207032382
 
 
-def fetch_groups(token):
-    response = requests.get(
-        f"{API_BASE_URL}/groups.get",
-        params={
-            "access_token": token,
-            "v": API_VERSION,
-            "extended": True,
-            "count": 10,
-        }
-    )
-    response.raise_for_status()
-    return response.json()
-
-
 def get_upload_url(token):
     response = requests.get(
         f"{API_BASE_URL}/photos.getWallUploadServer",
